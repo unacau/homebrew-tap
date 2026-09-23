458c909e54c9113dbb12ebe02d77f1c209c05394
Igor Ekishev <igorekishev92@gmail.com>
2026-09-23 23:37:05 +0200

    chore(cask): update xomsky to v1.1.5

--- Casks/xomsky.rb
+++ Casks/xomsky.rb
@@ -1,6 +1,6 @@
 cask "xomsky" do
-  version "1.1.4"
-  sha256 "1787061b74729c25564551ad2a7e76b79874c05124fec06a1b02eaba76fb65ab"
+  version "1.1.5"
+  sha256 "4aaa55fd871e493a9aaaa4a73e595f1c259f8c28d09812e13f8ba0322f16d540"
 
   url "https://github.com/unacau/mac-productivity-suite/releases/download/v#{version}/Xomsky.dmg"
   name "Xomsky"
